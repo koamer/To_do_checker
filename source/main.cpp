17a17ef2197533f41cf8ee67f6abb89021b9a4ca
koamer <tom9807@wp.pl>
2022-08-30 20:01:55 +0200

    Adding multithreading entry point

--- source/main.cpp
+++ source/main.cpp
@@ -1,40 +1,49 @@
 #include <iostream>
+#include <memory>
 #include <string>
-#include <curses.h>
 #include <filesystem>
 #include <vector>
 #include <algorithm>
 #include <utility>
 #include <map>
-#include <pthread.h>
+#include <thread>
 
+#include <curses.h>
 
 namespace fs = std::filesystem;
 using file_str_list = std::vector<std::string>;
 
-inline void usage() {
-	std::cout <<
-	"Usage: to_do_tool [path_to_directory_with_project]" <<
-	std::endl;
+inline void usage()
+{
+	std::cout << "Usage: to_do_tool [path_to_directory_with_project]" << std::endl;
 }
 
+// void split_and_run(file_str_list &file_list)
+// {
+// }
 bool check_file(std::string file_name)
 {
 	const std::vector<std::string> file_extension = {".cpp", ".hpp", ".c", ".h", ".py", ".sh"};
-	for(const auto& ext : file_extension) {
-		if(file_name.find(ext) != std::string::npos) {
+	for (const auto &ext : file_extension)
+	{
+		if (file_name.find(ext) != std::string::npos)
+		{
 			return true;
 		}
 	}
 	return false;
 }
 
-uint32_t find_files(fs::path& dir, file_str_list& file_list) {
-	uint32_t loaded_files = 0; 
-	for(const auto &file : fs::recursive_directory_iterator(dir)) {
-		if(file.is_regular_file() == true) {
-			auto cur_file = file.path();			
-			if(check_file(cur_file.filename().string()) == true) {
+uint32_t find_files(fs::path &dir, file_str_list &file_list)
+{
+	uint32_t loaded_files = 0;
+	for (const auto &file : fs::recursive_directory_iterator(dir))
+	{
+		if (file.is_regular_file() == true)
+		{
+			auto cur_file = file.path();
+			if (check_file(cur_file.filename().string()) == true)
+			{
 				file_list.push_back(cur_file.string());
 				loaded_files++;
 			}
@@ -43,49 +52,63 @@ uint32_t find_files(fs::path& dir, file_str_list& file_list) {
 	return loaded_files;
 }
 
-std::string find_todos(file_str_list& file_list) {
-	//std::string 
+std::string find_todos(file_str_list &file_list)
+{
+	// std::string
 	std::map<std::string, uintmax_t> file_sizes;
-	for(auto file : file_list) {
+	for (auto file : file_list)
+	{
 		file_sizes.insert(std::make_pair(file, fs::file_size(fs::path(file))));
 	}
-	split_and_run();
 
- }
+	const uint32_t threads_number = std::thread::hardware_concurrency();
 
-int main(int argc, char *argv[]) {
-	if(argc != 2) {
+	std::vector<std::thread> threads(threads_number);
+	for (auto &thread : threads)
+	{
+	}
+
+	return "";
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
 		usage();
 		return EXIT_FAILURE;
 	}
 
 	fs::path path_to_director;
 
-	if(std::string(".").compare(argv[1]) != 0) {
+	if (std::string(".").compare(argv[1]) != 0)
+	{
 		path_to_director = fs::current_path();
 	}
-	else {
-		path_to_director = fs::path(argv[1]); 
+	else
+	{
+		path_to_director = fs::path(argv[1]);
 	}
 
-	if(fs::exists(path_to_director) == false)
+	if (fs::exists(path_to_director) == false)
 	{
 		std::cerr << "Directory doesn't exists" << std::endl;
 		return EXIT_FAILURE;
 	}
 
 	file_str_list file_list;
-	if(find_files(path_to_director, file_list) == 0) {
+	if (find_files(path_to_director, file_list) == 0)
+	{
 		std::cerr << "Cannot find any file with source code."
-		<< "Supported lanugange: C++, C, Python, Shell Script" << std::endl;
+							<< "Supported lanugange: C++, C, Python, Shell Script" << std::endl;
 		return EXIT_FAILURE;
 	}
 
-	find_todos(file_list);
+	std::string to_dos = find_todos(file_list);
 
-	//initscr();
-	//cbreak();
-	//noecho();
-	//endwin();
+	// initscr();
+	// cbreak();
+	// noecho();
+	// endwin();
 	return 0;
 }
